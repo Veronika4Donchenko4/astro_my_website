a40ebbd57335412e588884105d2f4ed5d9ed533f
Hammad <hmdshfq@outlook.com>
2023-11-28 20:52:58 +0100

    fix: avatar image

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,7 @@ import avatar from '../assets/images/avatar.jpg';
 import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import { useMediaQuery } from '../util/useMediaQuery';
+import avatar from '/public/images/avatar.jpg';
 
 interface Props {
   imagePath: string;
@@ -9,29 +10,9 @@ interface Props {
 }
 
 const Nav = () => {
-  const [toggled, setToggled] = useState(false);
-  const matches = useMediaQuery('(min-width: 1280px)');
-
-  return (
-    <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
-      <svg
-        className="absolute bottom-0 left-1/2 -translate-x-1/2"
-        width="250"
-        height={4}
-        viewBox="0 0 250 4"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M2 2L428 2"
-          strokeWidth={2}
-          stroke="#CCCCCC"
-          strokeLinecap="round"
-        />
-      </svg>
-      <div>
-        <img src={avatar} alt="Profile picture Nika" />
-      </div>
+            <div>
+                <img src={avatar.src} alt='Profile picture Nika' />
+            </div>
 
       <h1 className="text-lg font-bold">
         <a href="/">Nika</a>
