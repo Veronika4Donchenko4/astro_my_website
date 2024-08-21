038dd2335560642d11f18a03b62c02a0d9dc47cf
Nika Donchenko <donchenko.veronika96@gmail.com>
2024-08-21 14:55:11 +0200

    Fix: Ensure client-side code executes only in the browser

--- src/pages/stars.js
+++ src/pages/stars.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const starfield = document.querySelector('.starfield');
-    const numStars = 100; // Количество звезд
-  
-    for (let i = 0; i < numStars; i++) {
-      const star = document.createElement('div');
-      star.className = 'star';
-      
-      // Задаем случайное положение и размер для каждой звезды
-      const size = Math.random() * 3 + 1; // Размер звезды от 1px до 4px
-      const leftPosition = Math.random() * 100; // Положение по горизонтали в процентах
-  
-      star.style.width = `${size}px`;
-      star.style.height = `${size}px`;
-      star.style.left = `${leftPosition}vw`;
-      
-      // Анимация для каждой звезды
-      const animationDuration = Math.random() * 5 + 5; // Продолжительность анимации от 5 до 10 секунд
-      star.style.animationDuration = `${animationDuration}s`;
-      
-      // Добавляем звезду на страницу
-      starfield.appendChild(star);
-    }
-  });
-  
+if (typeof window !== 'undefined') {
+	document.addEventListener('DOMContentLoaded', () => {
+		const starfield = document.querySelector('.starfield');
+		const numStars = 100;
+
+		for (let i = 0; i < numStars; i++) {
+			const star = document.createElement('div');
+			star.className = 'star';
+
+			const size = Math.random() * 3 + 1;
+			const leftPosition = Math.random() * 100;
+
+			star.style.width = `${size}px`;
+			star.style.height = `${size}px`;
+			star.style.left = `${leftPosition}vw`;
+
+			const animationDuration = Math.random() * 5 + 5;
+			star.style.animationDuration = `${animationDuration}s`;
+
+			starfield.appendChild(star);
+		}
+	});
+}
